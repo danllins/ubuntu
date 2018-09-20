3a308f1a645040a09c9e4968f8eec0668d34fa19
danillo.lsilva <danillolinssilva@gmail.com>
2018-09-20 20:16:31 -0300

    só o inicio da tentativa

--- tentando programar/main.py
+++ tentando programar/main.py
@@ -1,3 +1,17 @@
-print("oi mundo!")
-print("oi novamente!")
-print("oi novamente!")
+import math
+
+a = int(input("informe valor de a:"))
+b = int(input("informe valor de b:"))
+c = int(input("informe valor de c:"))
+
+delta = b * b - 4 * a * c
+
+print("delta:", delta)
+
+if delta >= 0:
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
+    print("Raiz x1:", x1)
+    print("Raiz x2:", x2)
+else:
+    print("Não existem raízes reais")
